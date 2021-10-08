21c4f4a14374292d904e46bce9b145defcaca5c7
Alexis A. Zarate <alexisarielzarate@icloud.com>
2021-10-08 00:36:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let string = "La pelota no se mancha";
+console.log(string);
+
+let saludo = "Hola, ";
+let nombre = "Pepe";
+let completo = saludo + nombre;
+console.log(completo);
+
+let multiple = saludo + saludo + saludo + nombre;
+console.log(multiple);
+
+let respuesta = saludo + "Estoy bien, Gracias ";
+console.log(respuesta);
